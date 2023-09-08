9141c5868425ed21874d5713c4ea11475a8f6696
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-09-07 22:05:44 -0300

    Add Populate Cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import MovieCard from './MovieCard';
 
@@ -7,24 +7,19 @@ import searchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com/?&apikey=b7400b8a'
 
-const movie1 = {
-    "Title": "Batman v Superman: Dawn of Justice",
-    "Year": "2016",
-    "imdbID": "tt2975590",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState(['']);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
-        searchMovies('Batman');
+        searchMovies('Game');
     }, []);
 
     return (
@@ -34,19 +29,31 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search your movie"
-                    value={"Superman"}
-                    onChange={() => { }}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img src={searchIcon}
                     alt="Search"
-                    onClick={() => { }}
+                    onClick={() => searchMovies(searchTerm)}
                 />
 
             </div>
 
-            <div className="container">
-                <MovieCard props={movie1} />
-            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) =>
+                                <MovieCard props={movie} />
+                            )}
+                        </div>
+
+                    )
+                    :
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+            }
 
         </div>
     );
